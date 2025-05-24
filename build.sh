c99b023a281ce66859fd5f8bbce7327827ed8674
Digitalguyco <youngkhito@gmail.com>
2025-05-24 21:55:52 +0100

    Fix: Correct Django settings module paths for build and runtime

--- build.sh
+++ build.sh
@@ -8,6 +8,9 @@ pip install -r convade_backend/requirements.txt
 # Install additional production dependencies
 pip install gunicorn psycopg2-binary whitenoise
 
+# Set Django settings for build commands
+export DJANGO_SETTINGS_MODULE=convade_backend.settings.production
+
 # Collect static files
 cd convade_backend
 python manage.py collectstatic --noinput
